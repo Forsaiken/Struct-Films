48a6d328157208192602b6146c581146ef8c0ba8
Forsaiken <for.vgmm@gmail.com>
2017-10-25 01:02:30 -0200

    Adicionado Struct filme

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+struct filme {
+
+	char	title[100];
+	int		genre[5];
+	char	produtora;
+	int		year;
+	int		imdb;
+
+};
+
 int main() {
 
 	printf("Hello World");
